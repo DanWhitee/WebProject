44685de197e2a2207ff29834e0282cb727c7b19e
Ginoice Team <75389091+DanWhitee@users.noreply.github.com>
2021-12-16 01:13:30 +0300

    default preset 1.1

---
+++ src/pages/index/ray/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="ray">
+    <p>Lorem</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Ray'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -3,8 +3,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    name: 'Home',
-  },
+    name: 'home',
+    component: () => import(/* webpackChunkName: "home" */ '@/pages/index/index.vue'),
+    children: [
+      {
+        path: 'ray',
+        name: 'ray',
+        component: () => import(/* webpackChunkName: "ray" */ '@/pages/index/ray/index.vue'),
+      }
+    ]
+  }
 ]
 
 const router = createRouter({

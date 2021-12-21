7bd0440fd442c5d6d830cbac534f349b36163f77
Ginoice Team <75389091+DanWhitee@users.noreply.github.com>
2021-12-21 13:56:19 +0300

    a

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import store from './store'
-import wow from '@/plugins/wow.js'
 
 import "animate.css"
+
 import "@/assets/themes/index.scss"
 import "@/assets/styles/index.scss"
 
 createApp(App)
     .use(store)
     .use(router)
-    .use(wow)
     .mount('#app')
